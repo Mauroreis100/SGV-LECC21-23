03ad72f173c0ac2e0883a37d1fbc8cbb75b0110a
Mauro Reis <69197927+Mauroreis100@users.noreply.github.com>
2023-05-21 15:56:21 +0200

    Operações de Clientes v1

---
+++ src/cliente/OperacoesClientes.java
@@ -0,0 +1,22 @@
+package cliente;
+
+import java.util.Vector;
+
+public class OperacoesClientes {
+	
+	//Cria o cliente e o coloca dentro do vector
+	public void criarCliente(Vector lista, Cliente cliente) {
+	}
+	
+	//Apagar pelo código
+	public void apagarCliente(Vector lista, Cliente cliente) {}
+	
+	//Deve pesquisar pelo código
+	public Cliente pesquisarCliente(Vector lista, int codigo) {
+		return null;
+	}
+	
+	//Actualizar Cliente
+	public void actualizarCliente(Vector lista, int Codigo) {}
+	
+}
