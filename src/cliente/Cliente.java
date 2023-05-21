c0adfffedd1082d7b5a2207de9e33ecf834c47fc
Mauro Reis <69197927+Mauroreis100@users.noreply.github.com>
2023-05-21 15:33:38 +0200

    Commit Inicial

---
+++ src/cliente/Cliente.java
@@ -0,0 +1,12 @@
+package cliente;
+
+import java.util.Calendar;
+
+public class Cliente {
+	private int id;
+	private String nome;
+	private Calendar data_criacao;
+	private int telefone;
+	private Vendas compras=null;
+	
+}
